2c0f955cbc3ac0d2accec93ee5735de90e7221a8
Bipul Neupane <bipul@crowdinvest.com>
2024-12-09 23:58:24 +1100

    Calculate working capital ratio

--- index.js
+++ index.js
@@ -18,20 +18,58 @@ try {
   let expenses = 0;
   let grossProfitMargin;
   let netProfitMargin;
-  let workingCapitalRatio = 0;
+  let workingCapitalRatio;
   let sales = 0;
+  let assets = 0;
+  let liabilities = 0;
 
   // Single iteration through the data array
   for (const entry of data) {
     const { account_category, account_type, value_type, total_value } = entry;
+
+    // Calculate revenue and expenses
     if (account_category === "revenue") {
       revenue += total_value;
     } else if (account_category === "expense") {
       expenses += total_value;
     }
 
+    // Calculate sales for 'grossProfitMargin'
     if (account_type === "sales" && value_type === "debit") {
       sales += total_value;
+    }
+    // Calculate assets for 'workingCapitalRatio'
+    if (account_category === "assets") {
+      if (
+        value_type === "debit" &&
+        ["current", "bank", "current_accounts_receivable"].includes(
+          account_type
+        )
+      ) {
+        assets += total_value;
+      } else if (
+        value_type === "credit" &&
+        ["current", "bank", "current_accounts_receivable"].includes(
+          account_type
+        )
+      ) {
+        assets -= total_value;
+      }
+    }
+
+    // Calculate liability for 'workingCapitalRatio'
+    if (account_category === "liability") {
+      if (
+        value_type === "credit" &&
+        ["current", "current_accounts_payable"].includes(account_type)
+      ) {
+        liabilities += total_value;
+      } else if (
+        value_type === "debit" &&
+        ["current", "current_accounts_payable"].includes(account_type)
+      ) {
+        liabilities -= total_value;
+      }
     }
   }
 
@@ -39,6 +77,10 @@ try {
   grossProfitMargin = revenue ? ((sales / revenue) * 100).toFixed(1) : 0;
   netProfitMargin = revenue
     ? (((revenue - expenses) / revenue) * 100).toFixed(1)
+    : 0;
+
+  workingCapitalRatio = liabilities
+    ? ((assets / liabilities) * 100).toFixed(1)
     : 0;
 
   // Print results
